cf3c15a9103a305421f7963b329ddcb2eda5afd0
KeyLimePie7 <124255249+KeyLimePie7@users.noreply.github.com>
2023-02-25 12:41:36 +0800

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
@@ -9,20 +8,6 @@ import ClaimComponent from './claimComponent/claimComponent.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <div>
       <ClaimComponent></ClaimComponent>
       </div>
--- client/src/claimComponent/claimComponent.js
+++ client/src/claimComponent/claimComponent.js
@@ -1,6 +1,43 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 
 const ClaimComponent = () => {
+    //call service to get all claims for this employee
+    //this is a sample employee claims list
+    let employeeClaimList = [
+        {
+            "ClaimID": 2010,
+            "InsuranceID": 1009,
+            "FirstName": "Martin",
+            "LastName": "Ong",
+            "ExpenseDate": "2022-07-14T08:00:00+08:00",
+            "Amount": 100.00,
+            "Purpose": "Dentist",
+            "FollowUp": 0,
+            "PreviousClaimID": null,
+            "Status": "Approved",
+            "LastEditedClaimDate": "2022-07-15T12:22:45+08:00"
+           },
+           {
+            "ClaimID": 2011,
+            "InsuranceID": 1008,
+            "FirstName": "John",
+            "LastName": "Tan",
+            "ExpenseDate": "2022-08-15T08:00:00+08:00",
+            "Amount": 100.00,
+            "Purpose": "Outpatient Claim",
+            "FollowUp": 0,
+            "PreviousClaimID": null,
+            "Status": "Approved",
+            "LastEditedClaimDate": "2022-08-16T19:35:53+08:00"
+           }
+    ];
+    const [employeeClaimsData, setEmployeeData] = useState();
+    useEffect(() => {
+        //code to get data from backend
+        setEmployeeData(employeeClaimList);
+      }, []); //when page loads, retrieve data from backend
 
   return (
     <div>
