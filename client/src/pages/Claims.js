7208c1a5f8abad6f48c07a03481d1b2e435a6b18
claris-toh <claristohh@gmail.com>
2023-02-25 16:54:19 +0800

    Remove invalid button

--- client/src/pages/Claims.js
+++ client/src/pages/Claims.js
@@ -2,12 +2,10 @@ import React from 'react';
 import '../App.css';
 import Dashboard from '../components/Dashboard';
 import ClaimComponent from '../components/claimComponent';
-import { Button } from '../components/Button';
 
 function Home () {
     return (
         <>
-        <Button buttonStyle='btn--back' path='/home'> Back </Button>
         <Dashboard />
         <ClaimComponent />
         </>
