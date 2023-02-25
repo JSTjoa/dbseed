82b9999520fc652240d844096420d54bbb9ba948
claris-toh <claristohh@gmail.com>
2023-02-25 11:28:52 +0800

    Update claim form

--- client/src/components/claimForm.js
+++ client/src/components/claimForm.js
@@ -1,54 +1,60 @@
-import { useState } from 'react'
+//import { useState } from 'react'
+import './ClaimForm.css';
 
-const claimForm = () => {
-    const [expenseDate, setDate] = useState('')
-    const [claimType, setType] = useState('')
-    const [purpose, setPurpose] = useState('')
-    const [amount, setAmount] = useState('')
+const ClaimForm = () => {
+    //const [expenseDate, setDate] = useState('')
+    //const [claimType, setType] = useState('')
+    //const [purpose, setPurpose] = useState('')
+    //const [amount, setAmount] = useState('')
+
+
+    // onChange={(e) => setDate(e.target.value)} 
+    
 
     return (
-        <form className='add-form' onSubmit={onSubmit}>
-            {/* Date: Expense Date */}
-            <div className='form-control'>
-                <label>Expense date</label>
-                <input 
-                    type='date'
-                    value={expenseDate}
-                    onChange={(e) => setDate(e.target.value)} 
-                />
-            </div>
-            {/* Dropdown: Insurance type */}
-            <div className='form-control'>
-                <label>Insurance type</label>
-                <input 
-                    type='select'
-                    placeholder="- Select -"
-                    value={claimType}
-                    onChange={(e) => setType(e.target.value)}  
-                />
-            </div>
-            {/* Text: Purpose */}
-            <div className='form-control'>
-                <label>Purpose</label>
-                <input
-                    type='text'
-                    checked={purpose}
-                    value={purpose}
-                    onChange={(e) => setPurpose(e.currentTarget.checked)}  
-                />
-            </div>
-            {/* Text: Amount */}
-            <div className='form-control'>
-                <label> Amount ($) </label>
-                <input
-                    type='text'
-                    value={amount}
-                    onChange={(e) => setAmount(e.currentTarget.checked)}  
-                />
-            </div>
-            <input type='submit' value='Save Task' className="btn btn-block"/>
-        </form>
+        <div className="container">
+            <form className='add-form'>
+                {/* Date: Expense Date */}
+                <div className='form-control'>
+                    <label>Expense date</label>
+                    <input 
+                        type='date'
+                        /* value={expenseDate}
+                        onChange={(e) => setDate(e.target.value)}  */
+                    />
+                </div>
+                {/* Dropdown: Insurance type */}
+                <div className='form-control'>
+                    <label>Insurance type</label>
+                    <input 
+                        type='select'
+                        placeholder="- Select -"
+                        /* value={claimType}  */ 
+                    />
+                </div>
+                {/* Text: Purpose */}
+                <div className='form-control'>
+                    <label>Purpose</label>
+                    <input
+                        type='text'
+                        placeholder="Key in an amount (in SGD)"
+                        /* value={purpose}
+                        onChange={(e) => setPurpose(e.currentTarget.checked)} */
+                    />
+                </div>
+                {/* Text: Amount */}
+                <div className='form-control'>
+                    <label> Amount ($) </label>
+                    <input
+                        type='text'
+                        /* value={amount}
+                        onChange={(e) => setAmount(e.currentTarget.checked)}  */ 
+                    />
+                </div>
+                <input type='submit' value='Save Task' className="btn btn-block"/>
+            </form>
+        </div>
     )
 }
 
-export default Form 
+export default ClaimForm 
