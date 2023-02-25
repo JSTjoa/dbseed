104eefc7c0a8c668d3e6efaaaf5fc1a711132ccf
KeyLimePie7 <124255249+KeyLimePie7@users.noreply.github.com>
2023-02-25 14:45:40 +0800

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import './components/claimComponentCSS.css'
 import Home from './pages/Home';
+import ClaimComponent from './components/claimComponent';
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path = '/' element= {<Home/>} />
+        <Route path = '/claims' element = {<ClaimComponent/>}></Route>
         </Routes>
       </Router>
     </>
