70498b037046e347a648bcafe2f26e194764e8ef
KeyLimePie7 <124255249+KeyLimePie7@users.noreply.github.com>
2023-02-25 14:00:19 +0800

    added faux service

--- client/src/components/claimComponent.js
+++ client/src/components/claimComponent.js
@@ -1,47 +1,23 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import claimComponentService from '../services/claimComponentService';
 
 
 const ClaimComponent = () => {
-    //call service to get all claims for this employee
-    //this is a sample employee claims list
-    let employeeClaimList = [
-        {
-            "ClaimID": 2010,
-            "InsuranceID": 1009,
-            "FirstName": "Martin",
-            "LastName": "Ong",
-            "ExpenseDate": "2022-07-14T08:00:00+08:00",
-            "Amount": 100.00,
-            "Purpose": "Dentist",
-            "FollowUp": 0,
-            "PreviousClaimID": null,
-            "Status": "Approved",
-            "LastEditedClaimDate": "2022-07-15T12:22:45+08:00"
-           },
-           {
-            "ClaimID": 2011,
-            "InsuranceID": 1008,
-            "FirstName": "John",
-            "LastName": "Tan",
-            "ExpenseDate": "2022-08-15T08:00:00+08:00",
-            "Amount": 100.00,
-            "Purpose": "Outpatient Claim",
-            "FollowUp": 0,
-            "PreviousClaimID": null,
-            "Status": "Pending",
-            "LastEditedClaimDate": "2022-08-16T19:35:53+08:00"
-           }
-    ];
-    const [employeeClaimData, setEmployeeData] = useState();
-    useEffect(() => {
-        //code to get data from backend
-        setEmployeeData(employeeClaimList);
-      }, []); //when page loads, retrieve data from backend
+  const [employeeClaimData, setEmployeeData] = useState();
+  useEffect(() => {
+      let test2 = claimComponentService.getClaimData() //code to get data from backend
+      setEmployeeData(test2);
+  }, []); //when page loads, retrieve data from backend
+
+  const onHandleEdit = () => {
+    alert('edit');
+  }
+    
 
   return (
     <div>
-      <h2>inside claim component</h2>
+      <h2>User Claims</h2>
       <table>
         <thead>
           <tr>
@@ -55,7 +31,7 @@ const ClaimComponent = () => {
           </tr>
         </thead>
         <tbody>
-          {employeeClaimList.map((data, index) => {
+          {employeeClaimData && employeeClaimData.map((data, index) => {
             return (
             <tr key={data.ClaimID}>
               <th>{data.ClaimID}</th>
@@ -65,7 +41,7 @@ const ClaimComponent = () => {
               <th>{data.Status}</th>
               <th>{data.LastEditedClaimDate}</th>
               <th>
-                {("Pending"===data.Status || "Rejected"===data.Status)? 'Edit'+' '+'Delete':'Nil'}
+                {("Pending"===data.Status || "Rejected"===data.Status) ? <button onClick={onHandleEdit}>Edit</button>+' '+'Delete':'Nil'}
               </th>
             </tr> 
             )         
