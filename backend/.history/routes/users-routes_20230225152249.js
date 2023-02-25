966496ee2bbdb3565cd25575a350764ad98d2e8b
RuiXiong2211 <ruixiong8@hotmail.com>
2023-02-25 15:23:26 +0800

    uppdate routes

--- backend/.history/routes/users-routes_20230225152249.js
+++ backend/.history/routes/users-routes_20230225152249.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controller/user");
 
-router.post('/createUser', usersController.createNewUser)
+router.post('/create', usersController.createNewUser)
 router.post('/login', usersController.logOutUser);
 
 module.exports = router;
