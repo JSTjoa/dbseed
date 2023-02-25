d521228e8efe18cfba725553c234df35142b3b6d
b-leong <65589225+b-leong@users.noreply.github.com>
2023-02-25 15:36:30 +0800

    editClaims
    editClaims

--- backend/controller/claims.js
+++ backend/controller/claims.js
@@ -1,5 +1,6 @@
 const Claims = require("../model/claims-model");
-const { default: mongoose } = require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
 
 let currentID = 0;
 const createClaim =  async (req, res) => {
@@ -21,6 +22,28 @@ const createClaim =  async (req, res) => {
         const newClaim = await claim.save();
         //const allUsers = await User.find({});
         res.status(201).json(newClaim);
+        currentID++;
+    } catch (err) {
+        console.log(err);
+        res.status(500).send();
+    }
+}
+
+const editClaim = async (req,res) => {
+    try {
+        Claims.findOneAndUpdate({ ClaimID : req.body.ClaimID }, {
+            InsuranceID : req.body.InsuranceID,
+            FirstName : req.body.FirstName,
+            LastName : req.body.LastName,
+            ExpenseDate : req.body.ExpenseDate,
+            Amount : req.body.Amount,
+            Purpose : req.body.Purpose,
+            FollowUp : req.body.FollowUp,
+            Status : req.body.Status,
+            LastEditedClaimDate : req.body.LastEditedClaimDate
+        }).then(res.status(200).json({
+                message: "Claim Update Successful"
+            }))
     } catch (err) {
         console.log(err);
         res.status(500).send();
@@ -28,5 +51,6 @@ const createClaim =  async (req, res) => {
 }
 
 module.exports = {
-    createClaim
+    createClaim,
+    editClaim
   };
