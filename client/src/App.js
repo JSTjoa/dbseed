fa6267741f9a41d651ec872633566780c83d9c5a
frisciliasultan <frisciliasultan@gmail.com>
2023-02-25 11:42:52 +0800

    context

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Login from "./components/Login";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/authentication";
 
 function App() {
     return (
