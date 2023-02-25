70a9938b54b36b831b7a2ca0c3cb4ec993b754bc
frisciliasultan <frisciliasultan@gmail.com>
2023-02-25 16:56:50 +0800

    merege

--- backend/controller/claims.js
+++ backend/controller/claims.js
@@ -3,20 +3,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 let currentID = 0;
-const createClaim =  async (req, res) => {
+const createClaim = async (req, res) => {
     try {
         const claim = new Claims({
-            ClaimID : currentID,
-            InsuranceID : req.body.InsuranceID,
-            FirstName : req.body.FirstName,
-            LastName : req.body.LastName,
-            ExpenseDate : req.body.ExpenseDate,
-            Amount : req.body.Amount,
-            Purpose : req.body.Purpose,
-            FollowUp : req.body.FollowUp,
-            PreviousClaimID : req.body.PreviousClaimID,
-            Status : req.body.Status,
-            LastEditedClaimDate : req.body.LastEditedClaimDate
+            ClaimID: currentID,
+            InsuranceID: req.body.InsuranceID,
+            FirstName: req.body.FirstName,
+            LastName: req.body.LastName,
+            ExpenseDate: req.body.ExpenseDate,
+            Amount: req.body.Amount,
+            Purpose: req.body.Purpose,
+            FollowUp: req.body.FollowUp,
+            PreviousClaimID: req.body.PreviousClaimID,
+            Status: req.body.Status,
+            LastEditedClaimDate: req.body.LastEditedClaimDate
         });
 
         const newClaim = await claim.save();
@@ -27,51 +27,56 @@ const createClaim =  async (req, res) => {
         console.log(err);
         res.status(500).send();
     }
-}
+};
 
-const editClaim = async (req,res) => {
+const editClaim = async (req, res) => {
     try {
-        Claims.findOneAndUpdate({ ClaimID : req.body.ClaimID }, {
-            InsuranceID : req.body.InsuranceID,
-            FirstName : req.body.FirstName,
-            LastName : req.body.LastName,
-            ExpenseDate : req.body.ExpenseDate,
-            Amount : req.body.Amount,
-            Purpose : req.body.Purpose,
-            FollowUp : req.body.FollowUp,
-            Status : req.body.Status,
-            LastEditedClaimDate : req.body.LastEditedClaimDate
-        }).then(res.status(200).json({
+        Claims.findOneAndUpdate(
+            { ClaimID: req.body.ClaimID },
+            {
+                InsuranceID: req.body.InsuranceID,
+                FirstName: req.body.FirstName,
+                LastName: req.body.LastName,
+                ExpenseDate: req.body.ExpenseDate,
+                Amount: req.body.Amount,
+                Purpose: req.body.Purpose,
+                FollowUp: req.body.FollowUp,
+                Status: req.body.Status,
+                LastEditedClaimDate: req.body.LastEditedClaimDate
+            }
+        ).then(
+            res.status(200).json({
                 message: "Claim Update Successful"
-            }))
+            })
+        );
     } catch (err) {
         console.log(err);
         res.status(500).send();
     }
-}
+};
 
-const editClaim = async (req,res) => {
-    try {
-        Claims.findOneAndUpdate({ ClaimID : req.body.ClaimID }, {
-            InsuranceID : req.body.InsuranceID,
-            FirstName : req.body.FirstName,
-            LastName : req.body.LastName,
-            ExpenseDate : req.body.ExpenseDate,
-            Amount : req.body.Amount,
-            Purpose : req.body.Purpose,
-            FollowUp : req.body.FollowUp,
-            Status : req.body.Status,
-            LastEditedClaimDate : req.body.LastEditedClaimDate
-        }).then(res.status(200).json({
-                message: "Claim Update Successful"
-            }))
-    } catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
-}
+// const editClaim = async (req,res) => {
+//     try {
+//         Claims.findOneAndUpdate({ ClaimID : req.body.ClaimID }, {
+//             InsuranceID : req.body.InsuranceID,
+//             FirstName : req.body.FirstName,
+//             LastName : req.body.LastName,
+//             ExpenseDate : req.body.ExpenseDate,
+//             Amount : req.body.Amount,
+//             Purpose : req.body.Purpose,
+//             FollowUp : req.body.FollowUp,
+//             Status : req.body.Status,
+//             LastEditedClaimDate : req.body.LastEditedClaimDate
+//         }).then(res.status(200).json({
+//                 message: "Claim Update Successful"
+//             }))
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).send();
+//     }
+// }
 
 module.exports = {
     createClaim,
     editClaim
-  };
+};
