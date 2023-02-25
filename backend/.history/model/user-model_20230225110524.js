926ee1e0145514ea8485cb9e9c12bae0252478e1
RuiXiong2211 <ruixiong8@hotmail.com>
2023-02-25 11:09:17 +0800

    add backbone

---
+++ backend/.history/model/user-model_20230225110524.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+var Schema = mongoose.Schema
+let userModelSchema = new Schema({
+    EmployeeID: {
+        type: Number,
+        unique: true,
+        required: true
+    },
+    Password: {
+        type: String,
+        required: true
+    },
+    FirstName: {
+        type: String
+    },
+    LastName: {
+        type: String
+    },
+    Age: {
+        type: Number
+    },
+    Role: {
+        type: Array
+    }
+})
