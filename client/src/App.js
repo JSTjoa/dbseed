5a8e76be1993944e9134f85480bcee9e11a0e9fd
frisciliasultan <frisciliasultan@gmail.com>
2023-02-25 13:11:51 +0800

    Move into component

--- client/src/App.js
+++ client/src/App.js
@@ -2,36 +2,40 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
+
+// Auth stuff
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/authentication";
-import { useNavigate } from "react-router-dom";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
-// Check for token to keep user logged in
-if (localStorage.jwtToken) {
-    // Set auth token header auth
-    const token = localStorage.jwtToken;
-    setAuthToken(token); // Set token for every request
-
-    // Decode token and get user info and exp
-    const decoded = jwt_decode(token);
-
-    // Check for expired token
-    const currentTime = Date.now() / 1000; // to get in milliseconds
-    if (decoded.exp < currentTime) {
-        // Logout user
-
-        // Redirect to login
-        window.location.href = "./login";
-    }
-
-    // Set user and isAuthenticated
-    store.dispatch(setCurrentUser(decoded, false));
-
-    // Set user academic info
-    store.dispatch(initialSettings());
-}
+// Redux stuff
+import store from "./store";
+import { connect } from "react-redux";
 
 function App() {
+    // Check for token to keep user logged in
+    if (localStorage.jwtToken) {
+        // Set auth token header auth
+        const token = localStorage.jwtToken;
+        setAuthToken(token); // Set token for every request
+
+        // Decode token and get user info and exp
+        const decoded = jwt_decode(token);
+
+        // Check for expired token
+        const currentTime = Date.now() / 1000; // to get in milliseconds
+        if (decoded.exp < currentTime) {
+            // Logout user
+            store.dispatch(logoutUser());
+
+            // Redirect to login
+            window.location.href = "./login";
+        }
+
+        // Set user and isAuthenticated
+        store.dispatch(setCurrentUser(decoded));
+    }
+
     return (
         <Router>
             <div className="App">
@@ -41,4 +45,8 @@ function App() {
     );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(App);
